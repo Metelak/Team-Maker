0f557b175b99ba27488e3ff1f7a83a3e223c8b66
coyote <megan.metelak@gmail.com>
2022-02-02 15:40:26 -0600

    playing around with function prompts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // packages needed for the application
+const { tsExpressionWithTypeArguments } = require('@babel/types');
 const fs = require('fs');
 const inquirer = require('inquirer');
 // connect HTML file creator
@@ -17,8 +18,68 @@ const inputChoices = () => {
     return inquirer.prompt(choice);
 }
 
+// engineer prompts/input
+const engineerInput = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the engineers ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineers email address?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineers GitHub username?',
+        },
+    ])
+    .then(({name, id, email, github}) => {
+        team.push(new Engineer(name, id, email, github));
+        completeTeam()
+    })
+};
+
+// intern prompts/input
+const internInput = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the interns name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the interns ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the interns email address?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the interns school?',
+        },
+    ])
+    .then(({name, id, email, school}) => {
+        team.push(new Intern(name, id, email, school));
+        completeTeam()
+    })
+};
+
 // initial prompts, starting with manager input
-const userQuestions = teamData => {
+const startQuestions = teamData => {
     console.log(`
     =======================================================
     Lets make your team, please start with the Team Manager
@@ -53,7 +114,7 @@ const userQuestions = teamData => {
     .then(employeeData => {
         teamData.employee.push(employeeData);
         if (employeeData.confirmAddEmployee) {
-            return userQuestions(teamData);
+            return startQuestions(teamData);
         } else {
             return teamData;
         }
