a0751f64fd555d11c75c72f6a1dd685447b34379
coyote <megan.metelak@gmail.com>
2022-02-02 14:41:55 -0600

    added package and generate sit connection

---
+++ index.js
@@ -0,0 +1,6 @@
+// packages needed for the application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// connect HTML file creator
+const generateSite = require('./utils/generate-site');
+
