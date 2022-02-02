790646d28620556e89f3b920365ffb845f31d550
coyote <megan.metelak@gmail.com>
2022-02-02 17:27:41 -0600

    manager test passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,8 +2,11 @@
 const Manager = require('../lib/Manager.js');
 
 test('creates a manager object', () =>{
-    const manager = new Manager(1);
+    const manager = new Manager('Pete', 10, "pete@fakemail.com", 1);
 
+    expect(manager.name).toBe('Pete');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
     expect(manager.office).toEqual(expect.any(Number));
 
     expect((manager.getRole())).toBe("Manager");
