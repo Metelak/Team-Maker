ebc25ba6eda24913ee6098fb95f257600b5656f9
coyote <megan.metelak@gmail.com>
2022-02-02 15:01:04 -0600

    initiated index.js

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const inquirer = require('inquirer');
 // connect HTML file creator
 const generateSite = require('./utils/generate-site');
 
+// have the user choose type of employee to input
+const inputChoices = () => {
+    const choice = [
+        {
+            type: 'list',
+            name: 'type',
+            message: "Pick which type of employee to add to the team",
+            choices: ["Engineer", "Intern"],
+        }
+    ];
+    return inquirer.prompt(choice);
+}
+
+
+// const userQuestions = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your name?',
+//         },
+//         {
+//             type: 'input',
+
+//         }
+//     ])
+// }
